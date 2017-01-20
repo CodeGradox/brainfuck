13704b39f72d9b86bbde6d93f24506f6f6c594ff
Magnus Åsrud <magnusasrud92@gmail.com>
2017-01-20 16:23:49 +0100

    Cleaned up code and made it usable in stable rust

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,63 @@
-#![feature(io)]
-
-use std::io::{Read, Error as IOError};
+use std::io::Read;
+use std::io::BufRead;
 use std::fs::File;
 use std::env;
 
-fn read_file(file: &str) -> Result<String, IOError> {
-	let mut file = try!(File::open(file));
-	let mut buf = String::new();
-	try!(file.read_to_string(&mut buf));
-	Ok(buf)
-}
-
 fn main() {
-	let mut tape = [0u8; 1000];
-	let mut head: usize = 0;
-	let mut buf_pos: usize = 0;
-	let stdin = std::io::stdin();
+    let mut tape = [0u8; 1000];
+    let mut head = 0;
+    let mut buf_pos = 0;
+    let stdin = std::io::stdin();
 
-	let buf: Vec<char> = env::args()
-		.nth(1)
-		.map(|s| read_file(&s)
-			.expect(&format!("\"{}\" not found", s))
-			.chars()
-			.collect())
-		.expect("No argument given");
+    let file = env::args().nth(1).expect("No argument given.");
+    let mut str_buf = String::new();
+    File::open(&file)
+        .and_then(|mut f| f.read_to_string(&mut str_buf))
+        .expect(&format!("Could not read {}", file));
+    let buf: Vec<char> = str_buf.chars().collect();
 
-	while buf_pos != buf.len() {
-		match buf[buf_pos] {
-			'+' => tape[head] = tape[head].wrapping_add(1),
-			'-' => tape[head] = tape[head].wrapping_sub(1),
-			'>' => head += 1,
-			'<' => head -= 1,
-			'.' => print!("{}", tape[head] as char),
-			',' => {
-				stdin.lock()
-					.chars()
-					.next()
-					.map(|x| tape[head] = x.expect("Error reading input") as u8);
-			},
-			'[' => {
-				if tape[head] == 0 {
-					let mut level = 0;
-					while buf_pos != buf.len() {
-						buf_pos += 1;
-						match buf[buf_pos] {
-							'[' => level += 1,
-							']' if level == 0 => break,
-							']' => level -= 1,
-							_ => {},
-						}
-					}
-				}
-			},
-			']' => {
-				if tape[head] != 0 {
-					let mut level = 0;
-					while buf_pos > 0 {
-						buf_pos -= 1;
-						match buf[buf_pos] {
-							']' => level += 1,
-							'[' if level == 0 => break,
-							'[' => level -= 1,
-							_ => {},
-						}
-					}
-				}
-			},
-			_ => { /* Ignore all other characters */ },
-		}
-		buf_pos += 1;
-	}
+    while buf_pos != buf.len() {
+        match buf[buf_pos] {
+            '+' => tape[head] = tape[head].wrapping_add(1),
+            '-' => tape[head] = tape[head].wrapping_sub(1),
+            '>' => head += 1,
+            '<' => head -= 1,
+            '.' => print!("{}", tape[head] as char),
+            ',' => {
+                let mut line = String::new();
+                stdin.lock().read_line(&mut line).expect("Error Reading input.");
+                line.chars().next().map(|x| tape[head] = x as u8).expect("Empty input.");
+            }
+            '[' => {
+                if tape[head] == 0 {
+                    let mut level = 0;
+                    while buf_pos != buf.len() {
+                        buf_pos += 1;
+                        match buf[buf_pos] {
+                            '[' => level += 1,
+                            ']' if level == 0 => break,
+                            ']' => level -= 1,
+                            _ => {}
+                        }
+                    }
+                }
+            }
+            ']' => {
+                if tape[head] != 0 {
+                    let mut level = 0;
+                    while buf_pos > 0 {
+                        buf_pos -= 1;
+                        match buf[buf_pos] {
+                            ']' => level += 1,
+                            '[' if level == 0 => break,
+                            '[' => level -= 1,
+                            _ => {}
+                        }
+                    }
+                }
+            }
+            _ => {} 
+        }
+        buf_pos += 1;
+    }
 }
